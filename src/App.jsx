ed055d2b8c1a7d286cec7cedb4a6170e2043430f
krisine <kristine.nyaga@student.moringaschool.com>
2023-11-20 16:19:34 +0300

    change route configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import './App.css'
 import Product from './components/Products/Product'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cart from './components/cart/cart';
 import Home from './components/home/Home'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: '/shop',
-    element: <Product />,
-  },
-    {
-    path: '/cart',
-    element: <Cart />,
-  }
-]);
 function App() {
   return (
     <>
-      <RouterProvider router={router}/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
     </>
   )
 }
